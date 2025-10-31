a4b4b45ec3f80380dc4def661a293238bdc4709a
usmanrangrez <usmaanrangrez@gmail.com>
2025-10-31 18:44:43 +0100

    Fix import statement for Position in CellNotFoundException

--- Homework1/StorageModule/exceptions/CellNotFoundException.java
+++ Homework1/StorageModule/exceptions/CellNotFoundException.java
@@ -1,11 +1,11 @@
 package Homework1.StorageModule.exceptions;
 
-import Homework1.StorageModule.model.Position;
 import Homework1.StorageModule.constants.ExceptionMessages;
+import Homework1.StorageModule.model.Position;
 
 public class CellNotFoundException extends Exception {
 
-    public CellNotFoundException(Homework2.StorageModule.model.Position position) {
+    public CellNotFoundException(Position position) {
         super(ExceptionMessages.CELL_NOT_FOUND + position);
     }
 
